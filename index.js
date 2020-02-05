c63d4feafd7c97a450e4bbaa817e5799a084f3af
Danilkashtan <36625284+Danilkashtan@users.noreply.github.com>
2020-02-05 17:35:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const isBrowser = typeof window !== "undefined" && window.fetch
+const fetch = isBrowser ? window.fetch : require("cross-fetch");
+
+function getRequest(url) {
+	return fetch(url, {
+		headers: {
+      "Authorization": "1223"
+    }
+	})
+		.then(response => {
+			if (response.status === 401) {
+      console.log(response.statusText);
+    } else {
+    	response.json()
+    	.then(json => {
+			console.log(json);
+			return json;
+		})
+		.catch(err => {
+	    console.log(err)
+  });
+			}
+		})
+		
+};
+
+getRequest('http://localhost:3000/api');
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, (err) => {
+	if (err) {
+        return console.log("Error: ", err)
+    }
+	console.log("Server is listening on port " + port)
+})
+
+app.get("/api", (req, res) => {
+	 if (  req.headers.authorization !== '123')        
+    return res.status(401).send() // Access denied.   
+
+  // Access granted...
+	res.send({method: "GET"})
+});
